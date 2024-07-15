f4a32bc9348b3f7337c28a1b65c8ac0db656b395
iamnubie <nguyenhongminh952@gmail.com>
2024-07-16 00:11:37 +0700

    category and brand form

---
+++ admin_area/insert_brands.php
@@ -0,0 +1,15 @@
+<form action="" method="post" class="mb-2">
+    <div class="input-group w-90 mb-3">
+        <span class="input-group-text bg-info" id="basic-addon1"><i 
+        class="fa-solid fa-receipt"></i></span>
+        <input type="text" class="form-control" name="cat_title" 
+        placeholder="Thêm thương hiệu" 
+        aria-label="brands" aria-describedby="basic-addon1">
+    </div>
+    <div class="input-group w-10 mb-3 m-auto">
+
+        <!-- <input type="submit" class="form-control bg-info" name="insert_cat"
+        value="Thêm Mục Hàng" > -->
+        <button class=" bg-info p-2 my-3 border-0">Thêm Thương Hiệu</button>
+    </div>
+</form>
